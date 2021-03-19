7a28af9baa5edfc9d3119378cb10364800538284
triyul22 <triyul22@niuitmo.ru>
2021-03-19 03:26:22 +0300

    add weather information from API

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ navigator.geolocation.getCurrentPosition(success, error, {
 })
 
 function success({ coords }) {
+    document.getElementById("current-flex-container").style.visibility = 'visible';
+    console.log(document.getElementById("current-flex-container"))
+    document.getElementById("current-loader").style.visibility = 'hidden';
     const { latitude, longitude } = coords
-    const position = [latitude, longitude]
-    console.log(position)
 
     const xhr = new XMLHttpRequest();
 
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
             const data = JSON.parse(this.responseText);
-            //console.log(this.responseText);
+            console.log(this.responseText);
             set_current_info(data);
         }
     });
@@ -31,9 +32,23 @@ function error({ message }) {
 
 function set_current_info(data) {
     const info = data["data"][0]
-    document.getElementById("current-city").innerHTML = info["city_name"]
-    document.getElementById("current-temp").innerHTML = info["temp"] + " &#176;C"
-    console.log(document.getElementsByClassName("current"))
 
-    console.log(data["data"])
+    const weather_icon = info["weather"]["icon"].substring(1)
+    const weather_list = document.getElementsByClassName("current")[0].children
+
+    document.getElementById("current-city").innerHTML = info["city_name"]
+    document.getElementById("current-img").src = "https://api.openweathermap.org/img/w/" + weather_icon + ".png"
+    document.getElementById("current-temp").innerHTML = info["temp"] + " &#176;C"
+
+    weather_list[0].children[1].innerHTML = info["wind_spd"] + "m/s, " + info["wind_cdir_full"]
+    weather_list[1].children[1].innerHTML = info["clouds"] + "%"
+    weather_list[2].children[1].innerHTML = info["pres"] + " hps"
+    weather_list[3].children[1].innerHTML = info["rh"] + "%"
+    weather_list[4].children[1].innerHTML = "[" + info["lat"] + ", " + info["lon"] + "]"
+}
+
+function add_city(el) {
+    console.log(el)
+    const favourite_cities = localStorage.getItem('favourite')
+    localStorage.setItem('favourite', [])
 }
