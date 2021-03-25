726686ba461fefe0ccc10c53be4013e5a348d7ef
triyul22 <triyul22@niuitmo.ru>
2021-03-25 23:25:28 +0300

    add favourite cities functionality

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ navigator.geolocation.getCurrentPosition(success, error, {
 
 function success({ coords }) {
     document.getElementById("current-flex-container").style.visibility = 'visible';
-    console.log(document.getElementById("current-flex-container"))
     document.getElementById("current-loader").style.visibility = 'hidden';
     const { latitude, longitude } = coords
 
@@ -13,7 +12,6 @@ function success({ coords }) {
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
             const data = JSON.parse(this.responseText);
-            console.log(this.responseText);
             set_current_info(data);
         }
     });
@@ -27,6 +25,7 @@ function success({ coords }) {
 }
 
 function error({ message }) {
+    // fill the html with default city
     console.log(message)
 }
 
@@ -48,7 +47,138 @@ function set_current_info(data) {
 }
 
 function add_city(el) {
-    console.log(el)
-    const favourite_cities = localStorage.getItem('favourite')
-    localStorage.setItem('favourite', [])
+    const city = document.getElementById("add-city").value
+
+    const favourite_cities = localStorage.getItem('favourite') !== null ?
+        JSON.parse(localStorage.getItem('favourite'))
+        : [];
+
+    favourite_cities.push(city)
+
+    localStorage.setItem('favourite', JSON.stringify(favourite_cities))
+}
+
+function load_favourite_cities(city_name){
+    const data = null;
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            fill_favourite_html(this.responseText);
+        }
+    });
+
+    xhr.open("GET", "https://community-open-weather-map.p.rapidapi.com/find?q=" + city_name + "&cnt=1&mode=null&lon=0&type=link%2C%20accurate&lat=0&units=imperial%2C%20metric");
+    xhr.setRequestHeader("x-rapidapi-key", "d71d9addf4msh78bd8a995732d4dp11d03djsnc36475894a13");
+    xhr.setRequestHeader("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com");
+
+    xhr.send(data);
+    console.log(this)
+}
+
+function fill_favourite_html(params){
+    const info = JSON.parse(params)["list"][0]
+    console.log(info)
+
+    const li = createElement(info)
+
+    document.getElementById("favourite-list").appendChild(li)
+
+}
+
+function createWeatherList(weather_info){
+    const info_arr = {
+        "Wind": weather_info["wind"]["speed"] + " m/s, " + weather_info["wind"]["deg"] + " deg",
+        "Cloudiness": weather_info["clouds"]["all"] + "%",
+        "Pressure": weather_info["main"]["pressure"] + " hps",
+        "Humidity": weather_info["main"]["humidity"] + "%",
+        "Coordinates": "[" + weather_info["coord"]["lat"].toFixed(2) + ", " +  weather_info["coord"]["lon"].toFixed(2) + "]"
+    }
+
+    const ul = document.createElement("ul")
+    ul.className = "list"
+    let li;
+    let span;
+    let p;
+
+    for (let key in info_arr) {
+        const li = document.createElement("li")
+        const span = document.createElement("span")
+        span.innerHTML = key
+        const p = document.createElement("p")
+        p.className = "description"
+        p.innerHTML = info_arr[key]
+        li.appendChild(span)
+        li.appendChild(p)
+
+        ul.appendChild(li)
+    }
+    return ul
+}
+
+function createElement(weather_info){
+    const li_element = document.createElement("li");
+    li_element.className = "favourite"
+    const div_main_container = document.createElement("div")
+    div_main_container.className = "flex-container"
+
+    const h3 = document.createElement("h3")
+    h3.className = "city"
+    h3.innerHTML = weather_info["name"]
+
+    const div_image = document.createElement("div")
+    div_image.className = "favourite-image"
+
+    const img = document.createElement("img")
+    img.src = "https://api.openweathermap.org/img/w/" + weather_info["weather"][0]["icon"] + ".png"
+    img.alt = "Weather icon"
+
+    div_image.appendChild(img)
+
+    const span = document.createElement("span")
+    span.className = "temperature favourite-temp"
+    let cur_temp = Math.floor(parseFloat(weather_info["main"]["temp"]) - 273.15)
+    span.innerHTML = cur_temp.toString() + " &#176;C"
+
+    const button = document.createElement("button")
+    button.className = "add-button"
+    button.innerHTML = "x"
+
+    div_main_container.appendChild(h3)
+    div_main_container.appendChild(div_image)
+    div_main_container.appendChild(span)
+    div_main_container.appendChild(button)
+
+    li_element.appendChild(div_main_container)
+
+    const div_list = document.createElement("div")
+    div_list.className = "weather-list"
+
+/*    const ul = document.createElement("ul")
+    ul.className = "list"
+
+    const li_wind = document.createElement("li")
+    const span_wind = document.createElement("span")
+    span_wind.innerHTML = "Wind"
+    const p_wind = document.createElement("p")
+    p_wind.className = "description"
+    p_wind.innerHTML = weather_info["wind"]["speed"] + " m/s, " + weather_info["wind"]["deg"] + " deg"
+    li_wind.appendChild(span_wind)
+    li_wind.appendChild(p_wind)
+
+    ul.appendChild(li_wind)*/
+    const ul = createWeatherList(weather_info)
+
+    div_list.appendChild(ul)
+    li_element.appendChild(div_list)
+
+    return li_element
+
+}
+
+if (localStorage.getItem('favourite') !== null){
+    JSON.parse(localStorage.getItem('favourite')).forEach(function (city) {
+        load_favourite_cities(city);
+    })
 }
